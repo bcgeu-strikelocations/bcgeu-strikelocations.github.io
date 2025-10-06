cd7495d4389ba2fbdc03582555ad20e87f1b5451
Sepehr-Sobhani <Sepehr-Sobhani@users.noreply.github.com>
2025-10-06 15:00:47 +0000

    deploy: 7dbdd7715655cbb9683d2368f4aa47cae8bf0de5

--- js/loadGeoJsonLayers.js
+++ js/loadGeoJsonLayers.js
@@ -1,5 +1,5 @@
 import { map } from './map-config.js';
-import { strikeIcon, noPicketIcon } from './icons.js';
+import { strikeIcon, noPicketIcon, rallyIcon } from './icons.js';
 import { createPopupContent } from './utils.js';
 
 const style_30k = {
@@ -17,7 +17,9 @@ export async function loadGeoJsonLayers() {
     const strikeLayer = L.geoJSON(points_data.features, {
         pointToLayer: (feature, latlng) => {
             let icon;
-            if (feature.properties.is_picket_line) {
+            if (feature.properties.go_to_the_rally) {
+                icon = rallyIcon;
+            } else if (feature.properties.is_picket_line) {
                 icon = strikeIcon;
             } else {
                 icon = noPicketIcon;
